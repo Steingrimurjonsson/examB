7109c046941c8815a4659127795a561bd20c28a3
Stein <steingrimurkj@gmail.com>
2020-01-14 13:52:52 +0100

    fixed login

--- src/main/java/utils/SetupTestUsers.java
+++ src/main/java/utils/SetupTestUsers.java
@@ -20,29 +20,29 @@ public class SetupTestUsers {
     // Also, either delete this file, when users are created or rename and add to .gitignore
     // Whatever you do DO NOT COMMIT and PUSH with the real passwords
 
-   // User user = new User("user", "pass");
+   User user = new User("user", "pass");
     User admin = new User("admin", "pass");
- //   User both = new User("user_admin", "pass");
+    User both = new User("user_admin", "pass");
 
     if(admin.getUserPass().equals("test")/*||user.getUserPass().equals("test")||both.getUserPass().equals("test")*/)
       throw new UnsupportedOperationException("You have not changed the passwords");
 
     em.getTransaction().begin();
-    //Role userRole = new Role("user");
+    Role userRole = new Role("user");
     Role adminRole = new Role("admin");
-    //user.addRole(userRole);
+    user.addRole(userRole);
     admin.addRole(adminRole);
-    //both.addRole(userRole);
-    //both.addRole(adminRole);
- //   em.persist(userRole);
+    both.addRole(userRole);
+    both.addRole(adminRole);
+  em.persist(userRole);
     em.persist(adminRole);
-    //em.persist(user);
+    em.persist(user);
     em.persist(admin);
-    //em.persist(both);
+    em.persist(both);
     em.getTransaction().commit();
-    //System.out.println("PW: " + user.getUserPass());
-    //System.out.println("Testing user with OK password: " + user.verifyPassword("pass"));
-    //System.out.println("Testing user with wrong password: " + user.verifyPassword("12345"));
+    System.out.println("PW: " + user.getUserPass());
+    System.out.println("Testing user with OK password: " + user.verifyPassword("pass"));
+    System.out.println("Testing user with wrong password: " + user.verifyPassword("12345"));
     System.out.println("Created TEST Users");
    
   }
